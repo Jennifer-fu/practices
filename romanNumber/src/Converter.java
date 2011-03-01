8da22da346165df4229af8a0642f3a434a1bae4f
unknown <sks@.(none)>
2011-03-01 21:41:46 +0800

    tasking 2: simple convert roman number to arabic number

--- romanNumber/src/Converter.java
+++ romanNumber/src/Converter.java
@@ -1,7 +1,7 @@
 import java.util.HashMap;
 
 public class Converter {
-    private final HashMap<Integer,String> numMap = new HashMap<Integer,String>(){
+    private final HashMap<Integer,String> arabicToRoman = new HashMap<Integer,String>(){
         {
             put(1,"I");
             put(5,"V");
@@ -12,7 +12,23 @@ public class Converter {
             put(1000,"M");
         }
     };
-    public String convert(int arabicNum) {
-        return numMap.get(arabicNum);
+
+    private final HashMap<String,Integer> romanToArabic = new HashMap<String,Integer>(){
+        {
+            put("I",1);
+            put("V",5);
+            put("X",10);
+            put("L",50);
+            put("C",100);
+            put("D",500);
+            put("M",1000);
+        }
+    };
+    public String convertArabicToRoman(int arabicNum) {
+        return arabicToRoman.get(arabicNum);
+    }
+
+    public int convertRomanToArabic(String romanNum) {
+        return romanToArabic.get(romanNum);
     }
 }
--- romanNumber/test/CovertTest.java
+++ romanNumber/test/CovertTest.java
@@ -5,20 +5,39 @@ import static org.junit.Assert.assertEquals;
 public class CovertTest {
     @Test
     public void one_should_convert_to_I() {
-        String romanOne = new Converter().convert(1);
+        String romanOne = new Converter().convertArabicToRoman(1);
         assertEquals("I", romanOne);
-        String romanFive = new Converter().convert(5);
+        String romanFive = new Converter().convertArabicToRoman(5);
         assertEquals("V", romanFive);
-        String romanTen = new Converter().convert(10);
+        String romanTen = new Converter().convertArabicToRoman(10);
         assertEquals("X", romanTen);
-        String romanFifty = new Converter().convert(50);
+        String romanFifty = new Converter().convertArabicToRoman(50);
         assertEquals("L", romanFifty);
-        String romanHundred = new Converter().convert(100);
+        String romanHundred = new Converter().convertArabicToRoman(100);
         assertEquals("C", romanHundred);
-        String romanFiveHundred = new Converter().convert(500);
+        String romanFiveHundred = new Converter().convertArabicToRoman(500);
         assertEquals("D", romanFiveHundred);
-        String romanOneThousand = new Converter().convert(1000);
+        String romanOneThousand = new Converter().convertArabicToRoman(1000);
         assertEquals("M", romanOneThousand);
+    }
+
+    @Test
+    public void I_should_convert_to_one() {
+        int one = new Converter().convertRomanToArabic("I");
+        assertEquals(1, one);
+        int five = new Converter().convertRomanToArabic("V");
+        assertEquals(5, five);
+        int ten = new Converter().convertRomanToArabic("X");
+        assertEquals(10, ten);
+        int fifty = new Converter().convertRomanToArabic("L");
+        assertEquals(50, fifty);
+        int hundred = new Converter().convertRomanToArabic("C");
+        assertEquals(100, hundred);
+        int fiveHundred = new Converter().convertRomanToArabic("D");
+        assertEquals(500, fiveHundred);
+        int thousand = new Converter().convertRomanToArabic("M");
+        assertEquals(1000, thousand);
+
     }
 
 }
