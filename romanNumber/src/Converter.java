999161f0520ab10fc7fdbbc83800bebfbdce8a36
unknown <sks@.(none)>
2011-03-01 22:51:28 +0800

    tasking 3: two roman number II->2, VI->6, IV->4

--- romanNumber/src/Converter.java
+++ romanNumber/src/Converter.java
@@ -1,34 +1,56 @@
 import java.util.HashMap;
 
 public class Converter {
-    private final HashMap<Integer,String> arabicToRoman = new HashMap<Integer,String>(){
+    private final HashMap<Integer, Character> arabicToRoman = new HashMap<Integer, Character>() {
         {
-            put(1,"I");
-            put(5,"V");
-            put(10,"X");
-            put(50,"L");
-            put(100,"C");
-            put(500,"D");
-            put(1000,"M");
+            put(1, 'I');
+            put(5, 'V');
+            put(10, 'X');
+            put(50, 'L');
+            put(100, 'C');
+            put(500, 'D');
+            put(1000, 'M');
         }
     };
 
-    private final HashMap<String,Integer> romanToArabic = new HashMap<String,Integer>(){
+    private final HashMap<Character, Integer> romanToArabic = new HashMap<Character, Integer>() {
         {
-            put("I",1);
-            put("V",5);
-            put("X",10);
-            put("L",50);
-            put("C",100);
-            put("D",500);
-            put("M",1000);
+            put('I', 1);
+            put('V', 5);
+            put('X', 10);
+            put('L', 50);
+            put('C', 100);
+            put('D', 500);
+            put('M', 1000);
         }
     };
-    public String convertArabicToRoman(int arabicNum) {
+
+    public char convertSingleArabicToRoman(int arabicNum) {
         return arabicToRoman.get(arabicNum);
     }
 
-    public int convertRomanToArabic(String romanNum) {
+    public int convertSingleRomanToArabic(char romanNum) {
         return romanToArabic.get(romanNum);
+    }
+
+
+    public int convertRomanToArabic(String multiRomanNum) {
+        char[] simpleRomanArray = multiRomanNum.toCharArray();
+        int result = 0;
+        for (int i = 0; i < simpleRomanArray.length;) {
+            int current = convertSingleRomanToArabic(simpleRomanArray[i]);
+            int after = 0;
+            if (i < simpleRomanArray.length - 1) {
+                after = convertSingleRomanToArabic(simpleRomanArray[i + 1]);
+            }
+            if (after > current) {
+                result += (after - current);
+                i += 2;
+            } else {
+                result += current;
+                i++;
+            }
+        }
+        return result;
     }
 }
--- romanNumber/test/CovertTest.java
+++ romanNumber/test/CovertTest.java
@@ -5,39 +5,52 @@ import static org.junit.Assert.assertEquals;
 public class CovertTest {
     @Test
     public void one_should_convert_to_I() {
-        String romanOne = new Converter().convertArabicToRoman(1);
-        assertEquals("I", romanOne);
-        String romanFive = new Converter().convertArabicToRoman(5);
-        assertEquals("V", romanFive);
-        String romanTen = new Converter().convertArabicToRoman(10);
-        assertEquals("X", romanTen);
-        String romanFifty = new Converter().convertArabicToRoman(50);
-        assertEquals("L", romanFifty);
-        String romanHundred = new Converter().convertArabicToRoman(100);
-        assertEquals("C", romanHundred);
-        String romanFiveHundred = new Converter().convertArabicToRoman(500);
-        assertEquals("D", romanFiveHundred);
-        String romanOneThousand = new Converter().convertArabicToRoman(1000);
-        assertEquals("M", romanOneThousand);
+        char romanOne = new Converter().convertSingleArabicToRoman(1);
+        assertEquals('I', romanOne);
+        char romanFive = new Converter().convertSingleArabicToRoman(5);
+        assertEquals('V', romanFive);
+        char romanTen = new Converter().convertSingleArabicToRoman(10);
+        assertEquals('X', romanTen);
+        char romanFifty = new Converter().convertSingleArabicToRoman(50);
+        assertEquals('L', romanFifty);
+        char romanHundred = new Converter().convertSingleArabicToRoman(100);
+        assertEquals('C', romanHundred);
+        char romanFiveHundred = new Converter().convertSingleArabicToRoman(500);
+        assertEquals('D', romanFiveHundred);
+        char romanOneThousand = new Converter().convertSingleArabicToRoman(1000);
+        assertEquals('M', romanOneThousand);
     }
 
     @Test
     public void I_should_convert_to_one() {
-        int one = new Converter().convertRomanToArabic("I");
+        int one = new Converter().convertSingleRomanToArabic('I');
         assertEquals(1, one);
-        int five = new Converter().convertRomanToArabic("V");
+        int five = new Converter().convertSingleRomanToArabic('V');
         assertEquals(5, five);
-        int ten = new Converter().convertRomanToArabic("X");
+        int ten = new Converter().convertSingleRomanToArabic('X');
         assertEquals(10, ten);
-        int fifty = new Converter().convertRomanToArabic("L");
+        int fifty = new Converter().convertSingleRomanToArabic('L');
         assertEquals(50, fifty);
-        int hundred = new Converter().convertRomanToArabic("C");
+        int hundred = new Converter().convertSingleRomanToArabic('C');
         assertEquals(100, hundred);
-        int fiveHundred = new Converter().convertRomanToArabic("D");
+        int fiveHundred = new Converter().convertSingleRomanToArabic('D');
         assertEquals(500, fiveHundred);
-        int thousand = new Converter().convertRomanToArabic("M");
+        int thousand = new Converter().convertSingleRomanToArabic('M');
         assertEquals(1000, thousand);
 
     }
 
+    @Test
+    public void II_should_convert_to_two() {
+        int two = new Converter().convertRomanToArabic("II");
+        assertEquals(2, two);
+        int six = new Converter().convertRomanToArabic("VI");
+        assertEquals(6, six);
+    }
+
+    @Test
+    public void IV_should_convert_to_four() {
+        int two = new Converter().convertRomanToArabic("IV");
+        assertEquals(4, two);
+    }
 }
